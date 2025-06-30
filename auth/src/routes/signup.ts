4db4043732e4cacd029680cdd7920b32b3f72568
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-06-30 21:19:09 +0300

    add custom error handling for request validation and database connection

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
+
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -13,9 +16,18 @@ router.post(
       .withMessage("Password must be between 4 and 20 characters"),
   ],
   (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
     const { email, password } = req.body;
 
-    res.send("Hi There!");
+    console.log("Creating a user", email, password);
+    throw new DatabaseConnectionError();
+
+    // res.send({});
   }
 );
 
