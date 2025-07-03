fd4aac22333e811c5759751cddd450772b4224b4
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-07-03 20:21:19 +0300

    feat: add User model with schema and buildUser function

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -28,7 +28,6 @@ app.use(errorHandler);
 const start = async () => {
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
-
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
