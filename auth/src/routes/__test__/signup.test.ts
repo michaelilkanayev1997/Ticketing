af6f454181da07cabcbc1174b0586bfd6aa031d0
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-07-08 19:36:08 +0300

    test: add duplicate email test case for signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,7 +1,6 @@
 import request from "supertest";
 
 import { app } from "../../app";
-import e from "express";
 
 it("returns a 201 on successful signup", async () => {
   return request(app)
@@ -42,5 +41,23 @@ it("returns a 400 with missing email and password", async () => {
   await request(app)
     .post("/api/users/signup")
     .send({ password: "password" })
+    .expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
     .expect(400);
 });
