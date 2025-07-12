b615b02aa4e830e23f524b8521c8d9f24935350b
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-07-12 16:31:20 +0300

    feat: fetch current user data on server and browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,27 @@
-const index = () => {
-  return <div>Landing Page</div>;
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing Page</h1>;
 };
 
-export default index;
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // We are on the server
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+    return data;
+  } else {
+    // We are on the browser
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+};
+
+export default LandingPage;
