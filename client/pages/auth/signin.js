26fd942e756739293009f34b89d8771cc78b3462
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-07-13 18:40:25 +0300

    feat: add sign in page

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -3,12 +3,12 @@ import Router from "next/router";
 
 import useRequest from "../../hooks/use-request";
 
-const signup = () => {
+const signin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { doRequest, errors } = useRequest({
-    url: "/api/users/signup",
+    url: "/api/users/signin",
     method: "post",
     body: { email, password },
     onSuccess: () => Router.push("/"),
@@ -22,7 +22,7 @@ const signup = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <h1>Sign Up</h1>
+      <h1>Sign In</h1>
       <div className="form-group">
         <label>Email Address</label>
         <input
@@ -41,9 +41,9 @@ const signup = () => {
         />
       </div>
       {errors}
-      <button className="btn btn-primary">Sign Up</button>
+      <button className="btn btn-primary">Sign In</button>
     </form>
   );
 };
 
-export default signup;
+export default signin;
