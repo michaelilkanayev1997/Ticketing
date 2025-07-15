41c6e304924bf52fe9d681e8ddc527618bb98aab
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-07-15 19:07:56 +0300

    feat: add signout page for user authentication

---
+++ client/pages/auth/signout.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import useRequest from "../../hooks/use-request";
+
+const signout = () => {
+  const router = useRouter();
+
+  const { doRequest } = useRequest({
+    url: "/api/users/signout",
+    method: "post",
+    body: {},
+    onSuccess: () => router.push("/"),
+  });
+
+  useEffect(() => {
+    doRequest();
+  }, []);
+
+  return <div>Signing you out...</div>;
+};
+
+export default signout;
