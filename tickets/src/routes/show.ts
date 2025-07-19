fdf153a9ce689885531767d3e226ebf1a0fb06d9
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-07-19 14:31:22 +0300

    add show ticket tests

--- tickets/src/routes/show.ts
+++ tickets/src/routes/show.ts
@@ -1,22 +1,18 @@
 import express, { Request, Response } from "express";
-import { NotFoundError, requireAuth } from "@michaelil/common";
+import { NotFoundError } from "@michaelil/common";
 
 import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
-router.get(
-  "/api/tickets/:id",
-  requireAuth,
-  async (req: Request, res: Response) => {
-    const ticket = await Ticket.findById(req.params.id);
+router.get("/api/tickets/:id", async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
 
-    if (!ticket) {
-      throw new NotFoundError();
-    }
-
-    res.send(ticket);
+  if (!ticket) {
+    throw new NotFoundError();
   }
-);
+
+  res.send(ticket);
+});
 
 export { router as showTicketRouter };
