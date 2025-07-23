fadad0824d31a684596e1ee717bac33cf61edf09
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-07-23 20:59:47 +0300

    add graceful client shutdown

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,6 +10,11 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
+
   const options = stan.subscriptionOptions().setManualAckMode(true);
 
   const subscription = stan.subscribe(
@@ -28,3 +33,6 @@ stan.on("connect", () => {
     msg.ack(); // Acknowledge the message
   });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
