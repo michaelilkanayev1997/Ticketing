7d8218b7a312988903779528e496c132ea840421
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-07-29 19:05:16 +0300

    publishing ticket creating in tickets service

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@michaelil/common";
 
 import { Ticket } from "../models/ticket";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
 
 const router = express.Router();
 
@@ -25,6 +26,14 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
+
+    // Publish an event saying that a ticket was created
+    // new TicketCreatedPublisher(client).publish({
+    //   id: ticket.id,
+    //   title: ticket.title,
+    //   price: ticket.price,
+    //   userId: ticket.userId,
+    // });
 
     res.status(201).send(ticket);
   }
