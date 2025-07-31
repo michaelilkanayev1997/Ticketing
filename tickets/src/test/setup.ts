4cb7ac3d9f25b1154e97c51fa9813db5e4fe6244
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-07-31 21:10:26 +0300

    Ensuring Mock invocations

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -22,6 +22,8 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+  jest.clearAllMocks();
+
   if (mongoose.connection.db) {
     const collections = await mongoose.connection.db.collections();
 
