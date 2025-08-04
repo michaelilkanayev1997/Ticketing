95fe2554885621abf407ffd5c2f71c551974069f
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-08-04 19:47:57 +0300

    feat: initialize orders service with Express, MongoDB, and NATS

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import "express-async-errors";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@michaelil/common";
+
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+
+app.set("trust proxy", true); // Trust the first proxy
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test", // Use secure cookies in production
+  })
+);
+
+app.use(currentUser); // Middleware to check if user is authenticated
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
