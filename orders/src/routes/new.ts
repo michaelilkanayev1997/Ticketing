c9f836df634cb6238ac65ed0aa6057d3b12a83f7
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-08-05 19:31:47 +0300

    feat: add validation and requireAuth to order creation route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@michaelil/common";
 
 const router = express.Router();
 
-router.post("/api/orders", (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Ticket ID must be provided"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
