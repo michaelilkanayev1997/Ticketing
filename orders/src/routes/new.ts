33650911ec7e60dbdd775d2edf346c7ebb969322
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-08-07 21:53:39 +0300

    refactor: streamline ticket reservation check using isReserved method

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -36,18 +36,8 @@ router.post(
     }
 
     // Make sure that this ticket is not already reserved
-    const existingOrder = await Order.findOne({
-      ticket: ticketId,
-      status: {
-        $in: [
-          OrderStatus.Created,
-          OrderStatus.AwaitingPayment,
-          OrderStatus.Complete,
-        ],
-      },
-    });
-
-    if (existingOrder) {
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
       return new BadRequestError("Ticket is already reserved");
     }
 
