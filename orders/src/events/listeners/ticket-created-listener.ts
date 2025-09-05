cc80c641414861b129d9e6bc76690c21dca14eae
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-09-05 13:26:02 +0300

    add onMessage implementation

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -8,5 +8,16 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
   queueGroupName = queueGroupName;
 
-  onMessage(data: TicketCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    const { id, title, price } = data;
+
+    const ticket = Ticket.build({
+      id,
+      title,
+      price,
+    });
+    await ticket.save();
+
+    msg.ack();
+  }
 }
