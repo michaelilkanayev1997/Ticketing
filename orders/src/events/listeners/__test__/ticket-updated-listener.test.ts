3da87007632f86890e975e5d71b7a37b541345c3
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-09-19 19:18:17 +0300

    Testing the ticket updated listener

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { TicketUpdatedEvent } from "@michaelil/common";
+
+import { natsWrapper } from "../../../nats-wrapper";
+import { TicketUpdatedListener } from "../ticket-updated-listener";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+  // Create a listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  // Create a fake data object
+  const data: TicketUpdatedEvent["data"] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: "new concert",
+    price: 999,
+    userId: "asdf",
+  };
+
+  // Create a fake message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
+
+it("finds, updates, and saves a ticket", async () => {
+  const { listener, data, ticket, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure a ticket was created!
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.title).toEqual(data.title);
+  expect(updatedTicket!.price).toEqual(data.price);
+  expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure ack function is called
+  expect(msg.ack).toHaveBeenCalled();
+});
