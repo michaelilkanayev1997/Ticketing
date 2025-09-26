5dde1fc994e633623f4e8cc4d35ea548246e00e6
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-09-26 18:19:59 +0300

    Queuing a job on event arrival

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -2,13 +2,22 @@ import { Listener, OrderCreatedEvent, Subjects } from "@michaelil/common";
 import { Message } from "node-nats-streaming";
 
 import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    console.log("Event data!", data);
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: 15 * 60 * 1000, // 15 minutes
+      }
+    );
+
     msg.ack();
   }
 }
