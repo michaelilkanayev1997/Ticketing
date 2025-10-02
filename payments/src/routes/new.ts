5cd65275a3736bdaee5161cc5f51d7ecfcc207a3
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-10-02 16:51:07 +0300

    bump @michaelil/common version to 1.0.14 across all packages

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,7 @@ import {
 
 import { Order } from "../models/order";
 import { stripe } from "../stripe";
+import { Payment } from "../models/payment";
 
 const router = express.Router();
 
@@ -36,12 +37,18 @@ router.post(
       throw new BadRequestError("Cannot pay for a cancelled order");
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: "usd",
       amount: order.price * 100,
       source: token,
       description: `Charge for order ${order.id}`,
     });
+
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
 
     res.status(201).send({ success: true });
   }
